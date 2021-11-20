5e22a2c4927513af4ae094f1a3b3054ae3d0083c
Brenton Beltrami <contact@brentonbeltrami.com>
2021-11-20 17:12:15 -0600

    cleaned up cottagestart script

--- .config/zshell/scripts/cottagestart.sh
+++ .config/zshell/scripts/cottagestart.sh
@@ -7,7 +7,8 @@ tmux send-keys "git status" Enter
 tmux select-pane -L
 tmux split-window
 tmux send-keys "prisma" Enter
+tmux select-pane -R
 open -a /Applications/Firefox.app
 open https://github.com/joincottage/cottage-app/projects/1
-
+/usr/bin/osascript -e 'tell application "Safari"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell"
 
