415ad1fb62709d41c540bab2fc5671808b1597f3
Brenton Beltrami <contact@brentonbeltrami.com>
2021-12-05 22:53:07 -0600

    Added docker to start script, added formatting

--- .config/zshell/scripts/cottagestart.sh
+++ .config/zshell/scripts/cottagestart.sh
@@ -7,9 +7,11 @@ tmux split-window -h
 tmux send-keys "git status" Enter
 tmux select-pane -L
 tmux split-window
-tmux send-keys "prisma" Enter
+tmux send-keys "npm run prisma-studio" Enter
+tmux split-window
+tmux send-keys "docker compose up" Enter
 tmux select-pane -R
 open -a /Applications/Firefox.app
 open https://github.com/joincottage/cottage-app/projects/1
-/usr/bin/osascript -e 'tell application "Safari"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell"
+#/usr/bin/osascript -e 'tell application "Safari"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell"
 
